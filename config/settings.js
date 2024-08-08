5f7fe1aff28b1e4f961b17aef9ffdd4dfdbc30b8
Graham Primm <gprimm@rise8.us>
2024-08-07 18:37:55 -0700

    Revert "initial commit"
    This reverts commit 95137334ac28f4e8aa6c0c34379eeadce1864632.

--- config/settings.js
+++
@@ -1,6 +0,0 @@
-//YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
-
-export const mongoConfig = {
-  serverUrl: 'mongodb://localhost:27017/',
-  database: 'streamlinedscheduler'
-};
