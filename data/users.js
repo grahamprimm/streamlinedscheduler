cf6b67860c7ab2e944a24cf6c0a7e864287336ab
vixthunderboy <67233684+vixthunderboy@users.noreply.github.com>
2024-08-20 10:55:20 -0400

    Update users.js
    Validation + changes to get log in working

--- data/users.js
+++ data/users.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import { users } from '../config/mongoCollections.js';
-import { isValidString, isValidPassword, isValidTimezone, isValidRole } from '../helpers.js';
+import { isValidString, isValidPassword, isValidEmail, isValidTimezone, isValidRole } from '../helpers.js';
+import {createSchedule, getScheduleById} from './schedules.js'
 
 const saltRounds = 16;
 
@@ -16,11 +17,11 @@ export const registerUser = async (
 
   firstName = isValidString(firstName, 2, 25, 'First Name');
   lastName = isValidString(lastName, 2, 25, 'Last Name');
-  email = isValidEmail(email, 5, 10, 'Email').toLowerCase();
+  email = isValidEmail(email, 1, 256, 'Email').toLowerCase();
   password = isValidPassword(password);
   timezone = isValidTimezone(timezone);
   // make sure schedule is null
-  schedule = null;
+  let schedule = null;
   
   role = isValidRole(role, ['admin', 'user']);
 
@@ -31,6 +32,8 @@ export const registerUser = async (
 
   const hashedPassword = await bcrypt.hash(password, saltRounds);
 
+  schedule = await createSchedule()
+
   const newUser = {
     firstName,
     lastName,
@@ -38,7 +41,7 @@ export const registerUser = async (
     password: hashedPassword,
     timezone,
     role,
-    schedule: createSchedule()
+    schedule
   };
 
   const insertResult = await usersCollection.insertOne(newUser);
@@ -52,12 +55,23 @@ export const registerUser = async (
 
 export const loginUser = async (email, password) => {
   
+  
+
   const usersCollection = await users();
   
-  email = isValidEmail(email, 8, 20, 'Username').toLowerCase(); // TODO: validate email
+  
+
+  email = isValidEmail(email, 1, 256, 'Email').toLowerCase(); // TODO: validate email
+
+  
+
   password = isValidPassword(password);
 
+  
+
   const user = await usersCollection.findOne({ email });
+
+  
 
   if (!user) {
     throw new Error('Either the email or password is invalid');
@@ -68,7 +82,11 @@ export const loginUser = async (email, password) => {
     throw new Error('Either the email or password is invalid');
   }
 
+  
+
   const { firstName, lastName, mail, timezone, schedule, role  } = user;
+
+  
 
   return { firstName, lastName, mail, timezone, schedule, role };
 };
