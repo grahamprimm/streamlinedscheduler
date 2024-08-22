d0364ef9de4bb559eb4dfdc4785e64db58113083
vixthunderboy <67233684+vixthunderboy@users.noreply.github.com>
2024-08-22 17:09:27 -0400

    Update mongoCollections.js
    Added "events" collection

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -17,3 +17,4 @@ const getCollectionFn = (collection) => {
 
 export const users = getCollectionFn('users');
 export const schedules = getCollectionFn('schedules');
+export const events = getCollectionFn('events')
